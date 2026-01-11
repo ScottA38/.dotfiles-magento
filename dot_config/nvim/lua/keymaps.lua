feececde4956a973680902045feccd52cfa13cb6
ScottA38 <simple.scott.94@googlemail.com>
2026-01-11 19:41:11 +0000

    nix-chezmoi - Remove unnecessary nvim Rust plugin (`langs.lua`)

--- dot_config/nvim/lua/keymaps.lua
+++ dot_config/nvim/lua/keymaps.lua
@@ -1,10 +1,7 @@
-vim.keymap.set(
-  "n",
-  "<C-w>N",
-  "<cmd>vnew<CR>",
-  { desc = "Open vertical window" }
-)
+vim.keymap.set("n", "<C-w>N", "<cmd>vnew<CR>", { desc = "Open vertical window" })
 
-vim.keymap.set('n', '<leader>tt', function() 
-  require("snacks").terminal.toggle()
-end, { desc = "[T]oggle [T]erminal"})
+vim.keymap.set("n", "<leader>tt", function()
+	require("snacks").terminal.toggle()
+end, { desc = "[T]oggle [T]erminal" })
+
+vim.keymap.set("n", "<leader>b", "<cmd>Rexplore<cmd>", { desc = "Return to netrw from editing file (or vice-versa)" })
