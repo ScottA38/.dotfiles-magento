c79c709de937a92d699449cbf9fac7a68202eb1d
ScottA38 <simple.scott.94@googlemail.com>
2026-03-20 13:58:58 +0000

    > nix-chezmoi - Revert nvim keymap change

--- dot_config/nvim/lua/keymaps.lua
+++ dot_config/nvim/lua/keymaps.lua
@@ -1,4 +1,27 @@
-vim.keymap.set("n", "<leader>ot", ":tabnew term://zsh", { desc = "[T]oggle [T]erminal" })
-vim.keymap.set("n", "<leader>u", "<cmd>Rexplore<CR>", { desc = "Return to netrw from editing file (or vice-versa)" })
-vim.keymap.set("n", "<leader>ee", "<cmd>Explore<CR>", { desc = "Return to netrw from editing file (or vice-versa)" })
-vim.keymap.set("n", "<leader>nh", "<CR> :noh<CR>", { desc = "Un-highlight hl search matches" })
+vim.keymap.set(
+  "n",
+  "<leader>tt",
+  function() require("snacks").terminal.toggle() end,
+  { desc = "[T]oggle [T]erminal" }
+)
+
+vim.keymap.set(
+  "n",
+  "<leader>u",
+  "<cmd>Rexplore<CR>",
+  { desc = "Return to netrw from editing file (or vice-versa)" }
+)
+
+vim.keymap.set(
+  "n",
+  "<leader>ee",
+  "<cmd>Explore<CR>",
+  { desc = "Return to netrw from editing file (or vice-versa)" }
+)
+
+vim.keymap.set(
+  "n",
+  "<leader>nh",
+  "<CR> :noh<CR>",
+  { desc = "Un-highlight hl search matches" }
+)
