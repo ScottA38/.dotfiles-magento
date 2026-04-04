32770fb26678ad962168f6c32e75c8c280b85cbf
ScottA38 <simple.scott.94@googlemail.com>
2026-04-04 13:27:44 +0100

    > nix-chezmoi - Add nvim keymap for JSON auto-formatting

--- dot_config/nvim/lua/keymaps.lua
+++ dot_config/nvim/lua/keymaps.lua
@@ -1,27 +1,11 @@
-vim.keymap.set(
-  "n",
-  "<leader>tt",
-  function() require("snacks").terminal.toggle() end,
-  { desc = "[T]oggle [T]erminal" }
-)
+vim.keymap.set("n", "<leader>tt", function()
+	require("snacks").terminal.toggle()
+end, { desc = "[T]oggle [T]erminal" })
 
-vim.keymap.set(
-  "n",
-  "<leader>u",
-  "<cmd>Rexplore<CR>",
-  { desc = "Return to netrw from editing file (or vice-versa)" }
-)
+vim.keymap.set("n", "<leader>u", "<cmd>Rexplore<CR>", { desc = "Return to netrw from editing file (or vice-versa)" })
 
-vim.keymap.set(
-  "n",
-  "<leader>ee",
-  "<cmd>Explore<CR>",
-  { desc = "Return to netrw from editing file (or vice-versa)" }
-)
+vim.keymap.set("n", "<leader>ee", "<cmd>Explore<CR>", { desc = "Return to netrw from editing file (or vice-versa)" })
 
-vim.keymap.set(
-  "n",
-  "<leader>nh",
-  "<CR> :noh<CR>",
-  { desc = "Un-highlight hl search matches" }
-)
+vim.keymap.set("n", "<leader>nh", "<CR> :noh<CR>", { desc = "Un-highlight hl search matches" })
+
+vim.keymap.set("n", "<leader>jq", ": %!jq .<CR>", { desc = "Format JSON file using `jq` magic." })
